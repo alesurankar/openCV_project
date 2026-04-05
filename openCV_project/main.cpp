98fbe19a1782d2d6c7c9d3f9f14749952121f5cd
ales2026 <ale.urankar@gmail.com>
2026-04-05 09:40:22 +0200

    text

--- openCV_project/main.cpp
+++ openCV_project/main.cpp
@@ -12,6 +12,8 @@ int main()
 	circle(img, Point(256,256), 155, Scalar(0, 69, 255), FILLED);
 	rectangle(img, Point(130, 226), Point(382, 286), Scalar(255, 255, 255), FILLED);
 	line(img, Point(130, 296), Point(382, 296), Scalar(255, 255, 255), 2);
+	
+	putText(img, "some text", Point(137, 262), FONT_HERSHEY_DUPLEX, 1.3, Scalar(0, 69, 255), 2);
 
 	imshow("image", img);
 	waitKey(0);
