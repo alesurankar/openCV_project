f23929ec5db331c90adce80581023a5b230c04f4
ales2026 <ale.urankar@gmail.com>
2026-04-05 09:33:49 +0200

    filled  & rectangle

--- openCV_project/main.cpp
+++ openCV_project/main.cpp
@@ -9,7 +9,8 @@ int main()
 
 	Mat img(512, 512, CV_8UC3, Scalar(250,250,250));
 
-	circle(img, Point(256,256), 155, Scalar(0, 69, 255), 10);
+	circle(img, Point(256,256), 155, Scalar(0, 69, 255), FILLED);
+	rectangle(img, Point(130, 226), Point(382, 286), Scalar(255, 255, 255), FILLED);
 
 	imshow("image", img);
 	waitKey(0);
