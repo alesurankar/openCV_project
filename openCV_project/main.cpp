bdddbc9fa74f9d6889dfb09476728fa137253763
ales2026 <ale.urankar@gmail.com>
2026-04-05 09:20:20 +0200

    crop

--- openCV_project/main.cpp
+++ openCV_project/main.cpp
@@ -9,16 +9,18 @@ int main()
 
 	std::string path = "resources/gal2.png";
 	Mat img = imread(path);
-	Mat imgResize1;
-	Mat imgResize2;
+	Mat imgResize1, imgResize2, imgCrop;
 
 	//std::cout << img.size << "/n";
 	resize(img, imgResize1, Size(640, 640));
 	resize(img, imgResize2, Size(), 0.4, 0.4);
+	Rect roi(100, 100, 300, 300);
+	imgCrop = img(roi);
 
 	imshow("image", img);
 	imshow("image Resize1", imgResize1);
 	imshow("image Resize2", imgResize2);
+	imshow("image Crop", imgCrop);
 	waitKey(0);
 	return 0;
 }
