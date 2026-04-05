7aef44b91ebf6808554887dc0b35033e5344aa9d
ales2026 <ale.urankar@gmail.com>
2026-04-05 09:30:01 +0200

    draw circle

--- openCV_project/main.cpp
+++ openCV_project/main.cpp
@@ -7,20 +7,11 @@ int main()
 {
 	utils::logging::setLogLevel(utils::logging::LOG_LEVEL_ERROR);
 
-	std::string path = "resources/gal2.png";
-	Mat img = imread(path);
-	Mat imgResize1, imgResize2, imgCrop;
+	Mat img(512, 512, CV_8UC3, Scalar(250,250,250));
 
-	//std::cout << img.size << "/n";
-	resize(img, imgResize1, Size(640, 640));
-	resize(img, imgResize2, Size(), 0.4, 0.4);
-	Rect roi(100, 100, 300, 300);
-	imgCrop = img(roi);
+	circle(img, Point(256,256), 155, Scalar(0, 69, 255), 10);
 
 	imshow("image", img);
-	imshow("image Resize1", imgResize1);
-	imshow("image Resize2", imgResize2);
-	imshow("image Crop", imgCrop);
 	waitKey(0);
 	return 0;
 }
