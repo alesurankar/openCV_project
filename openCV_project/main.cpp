6172ed4fdfa1014a8470d9f87525029acd0e85d1
ales2026 <ale.urankar@gmail.com>
2026-04-05 09:10:30 +0200

    canny, dilation, erode

--- openCV_project/main.cpp
+++ openCV_project/main.cpp
@@ -9,15 +9,22 @@ int main()
 
 	std::string path = "resources/gal2.png";
 	Mat img = imread(path);
-	Mat imgGray;
-	Mat imgBlur;
+	Mat imgGray, imgBlur, imgCanny, imgDil, imgErode;
 
 	cvtColor(img, imgGray, COLOR_BGR2GRAY);
 	GaussianBlur(img, imgBlur, Size(7,7), 5, 0);
+	Canny(imgBlur, imgCanny, 25, 75);
+
+	Mat kernel = getStructuringElement(MORPH_RECT, Size(5, 5));
+	dilate(imgCanny, imgDil, kernel);
+	erode(imgDil, imgErode, kernel);
 
 	imshow("image", img);
 	imshow("image Gray", imgGray);
 	imshow("image Blur", imgBlur);
+	imshow("image Canny", imgCanny);
+	imshow("image Dilation", imgDil);
+	imshow("image Erode", imgErode);
 	waitKey(0);
 	return 0;
 }
