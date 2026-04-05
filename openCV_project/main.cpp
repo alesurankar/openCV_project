487a901d7350510207d4c5b7caeda13d67712fc3
ales2026 <ale.urankar@gmail.com>
2026-04-05 08:51:43 +0200

    convert image to gray

--- openCV_project/main.cpp
+++ openCV_project/main.cpp
@@ -5,15 +5,14 @@ int main()
 {
 	cv::utils::logging::setLogLevel(cv::utils::logging::LOG_LEVEL_ERROR);
 
-	cv::VideoCapture cap(0);
-	cv::Mat img;
+	std::string path = "resources/gal2.png";
+	cv::Mat img = cv::imread(path);
+	cv::Mat imgGray;
 
-	while (true) {
-		cap.read(img);
-
-		cv::imshow("image", img);
-		cv::waitKey(1);
-	}
+	cv::cvtColor(img, imgGray, cv::COLOR_BGR2GRAY);
+	cv::imshow("image", img);
+	cv::imshow("image Gray", imgGray);
+	cv::waitKey(0);
 	return 0;
 }
 
