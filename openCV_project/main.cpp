05e0c3cf912ba17707aeb46120e9a01471c47b8d
ales2026 <ale.urankar@gmail.com>
2026-04-05 07:34:59 +0200

    loading image

--- openCV_project/main.cpp
+++ openCV_project/main.cpp
@@ -1,16 +1,13 @@
-#include <opencv2/opencv.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 #include <iostream>
 
-int main() {
-    cv::utils::logging::setLogLevel(cv::utils::logging::LOG_LEVEL_ERROR);
-    cv::Mat img = cv::imread("images/gal2.png");
-
-    if (img.empty()) {
-        std::cout << "Image not found!\n";
-        return -1;
-    }
-
-    cv::imshow("My Image", img);
-    cv::waitKey(0);
-    return 0;
+void main()
+{
+	std::string path = "resources/gal2.png";
+	cv::Mat img = cv::imread(path);
+	cv::imshow("image", img);
+	cv::waitKey(0);
 }
+
