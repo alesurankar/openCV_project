891995a302b55c94a6aaaf5a56527d06861e2ece
ales2026 <ale.urankar@gmail.com>
2026-04-05 16:18:20 +0200

    face detection on webcam

--- openCV_project/main.cpp
+++ openCV_project/main.cpp
@@ -3,28 +3,29 @@
 
 using namespace cv;
 
-
 int main()
 {
 	utils::logging::setLogLevel(utils::logging::LOG_LEVEL_ERROR);
 
-	std::string path = "resources/face.png";
-	Mat img = imread(path);
+	VideoCapture cap(0);
+	Mat img;
 
 	CascadeClassifier faceCascade;
 	faceCascade.load("resources/haarcascade_frontalface_default.xml");
-
-	if (faceCascade.empty()) { std::cout << "XML file not loaded" << std::endl; }
-
-	std::vector<Rect> faces;
-	faceCascade.detectMultiScale(img, faces, 1.1, 10);
-
-	for (int i = 0; i < faces.size(); i++) {
-		rectangle(img, faces[i].tl(), faces[i].br(), Scalar(255, 0, 255), 3);
+	if (faceCascade.empty()) { 
+		std::cout << "XML file not loaded" << std::endl; 
 	}
 
-	imshow("image", img);
-	waitKey(0);
+	while (true) {
+		cap.read(img);
+		std::vector<Rect> faces;
+		faceCascade.detectMultiScale(img, faces, 1.1, 10);
+
+		for (int i = 0; i < faces.size(); i++) {
+			rectangle(img, faces[i].tl(), faces[i].br(), Scalar(255, 0, 255), 3);
+		}
+		imshow("image", img);
+		waitKey(1);
+	}
 	return 0;
 }
-
