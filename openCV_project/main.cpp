8a279a497f86805e137cf7abdf79a17ecf173021
ales2026 <ale.urankar@gmail.com>
2026-04-05 09:36:14 +0200

    line

--- openCV_project/main.cpp
+++ openCV_project/main.cpp
@@ -11,6 +11,7 @@ int main()
 
 	circle(img, Point(256,256), 155, Scalar(0, 69, 255), FILLED);
 	rectangle(img, Point(130, 226), Point(382, 286), Scalar(255, 255, 255), FILLED);
+	line(img, Point(130, 296), Point(382, 296), Scalar(255, 255, 255), 2);
 
 	imshow("image", img);
 	waitKey(0);
